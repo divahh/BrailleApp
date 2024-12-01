47702baec5e1a6add98c8feea338671910f8bb95
diva <divahalimahhasan@gmail.com>
2024-12-02 06:54:54 +0700

    updating 3 fragment and making xml

--- app/src/main/java/com/example/brailleapp/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/brailleapp/ui/game/GameFragment.kt
@@ -1,4 +1,4 @@
-package com.example.brailleapp.ui.notifications
+package com.example.brailleapp.ui.game
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.brailleapp.databinding.FragmentNotificationsBinding
+import com.example.brailleapp.databinding.FragmentGameBinding
 
-class NotificationsFragment : Fragment() {
+class GameFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentGameBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val gameViewModel =
+            ViewModelProvider(this)[GameViewModel::class.java]
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.tvGame
+        gameViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/brailleapp/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/brailleapp/ui/game/GameViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.brailleapp.ui.notifications
+package com.example.brailleapp.ui.game
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class GameViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
