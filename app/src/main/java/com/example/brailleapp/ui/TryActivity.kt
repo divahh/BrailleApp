f04ceaca0d4a1aad2b22b79c9a83721098d88851
siskakhnnisa <siskakhnnisa@gmail.com>
2024-12-10 18:58:20 +0700

    layout scan (not yet fix)

---
+++ app/src/main/java/com/example/brailleapp/ui/TryActivity.kt
@@ -0,0 +1,15 @@
+package com.example.brailleapp.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.brailleapp.R
+
+class TryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+    }
+
+}
